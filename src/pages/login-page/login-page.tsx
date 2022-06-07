2efecd88c9d3a9f476328ce4d40f6a8c621ee887
Alexander Tarasov <tarassov.al@gmail.com>
2022-06-07 13:42:50 +0300

    linter fixed

---
+++ src/pages/login-page/login-page.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+import { FC } from "react";
+const LoginPage: FC = () => {
+	return (
+		<>
+			<Typography>Login</Typography>
+		</>
+	);
+};
+
+export default LoginPage;
