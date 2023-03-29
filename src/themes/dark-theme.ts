e03e8f6da3ca5e53f1b1c86a2954e7d501dcf8b0
Alexander Tarasov <tarassov.al@gmail.com>
2023-03-29 20:37:35 +0300

    dev-64 hides dark heme

--- src/themes/dark-theme.ts
+++ src/themes/dark-theme.ts
@@ -1,23 +1,26 @@
 import { blue, deepPurple, grey, red, teal } from "@mui/material/colors";
+import lightTheme from "./light-theme";
 
-const darkTheme = {
-	palette: {
-		// mode: "dark",
-		primary: {
-			main: deepPurple["A100"],
-		},
-		secondary: teal,
-		neutral: {
-			main: grey["A400"],
-			dark: grey["800"],
-			light: grey["A100"],
-		},
-		info: {
-			main: blue["A200"],
-			dark: blue["A400"],
-			light: blue["A100"],
-		},
-	},
-};
+const darkTheme = lightTheme;
+
+// 	{
+// 	palette: {
+// 		// mode: "dark",
+// 		primary: {
+// 			main: deepPurple["A100"],
+// 		},
+// 		secondary: teal,
+// 		neutral: {
+// 			main: grey["A400"],
+// 			dark: grey["800"],
+// 			light: grey["A100"],
+// 		},
+// 		info: {
+// 			main: blue["A200"],
+// 			dark: blue["A400"],
+// 			light: blue["A100"],
+// 		},
+// 	},
+// };
 
 export default darkTheme;
