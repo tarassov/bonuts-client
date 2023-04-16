2efc145240c63c41ed2a986681ce31c3bffccede
Alexander Tarasov <tarassov.al@gmail.com>
2023-04-16 10:51:10 +0300

    dev-64 migrate to vite

---
+++ vitest.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import * as path from "path";
+export default defineConfig({
+	plugins: [react({ fastRefresh: false }), tsconfigPaths()],
+	worker: {
+		plugins: [react()],
+	},
+	resolve: {
+		alias: {
+			// scss: path.resolve(__dirname, 'src/scss')
+		},
+	},
+	test: {
+		setupFiles: ["./src/__tests__/config/setup.ts"],
+		environment: "jsdom",
+		globals: true,
+		transformMode: {
+			web: [/\.[jt]sx$/],
+		},
+		//  deps: { experimentalOptimizer: { include: ['react-abac'], enabled: true } }
+	},
+});
