27e147e07b9a373ffce01594f2afeb66bd03092a
Alexander Tarasov <tarassov.al@gmail.com>
2023-07-02 02:08:08 +0300

    feat/dev-85 fixes donut edit form

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ export const API_URL = "https://api.bonuts.ru/api/v1/";
 
 export const API_URL_LOCAL = "http://localhost:3000/api/v1/";
 
-export const USE_LOCAL_API = true;
+export const USE_LOCAL_API = false;
 
 // turn this on if you want RTK Query to perform autoupdate
 export const USE_POLLING_INTERVAL = false;
