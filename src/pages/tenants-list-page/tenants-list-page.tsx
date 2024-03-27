e5d44c87eb77b76aedeae6cfae3dc97aecae1063
Alexander Tarasov <tarassov.al@gmai.com>
2024-03-28 00:27:14 +0300

    feat/dev-182 beta for 3CX for tenants list

--- src/pages/tenants-list-page/tenants-list-page.tsx
+++ src/pages/tenants-list-page/tenants-list-page.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { AccessibleTenantList } from "components/tenant/accessible-tenant-list/accessible-tenant-list";
 import { UserTenantsList } from "components/tenant/user-tenant-list/user-tenants-list";
 import { UserInvitationsList } from "components/invitation/user-invitations-list";
+import { Messenger } from "components/3cx/messenger";
 
 export const TenantsListPage: FC = () => {
 	return (
@@ -9,6 +10,7 @@ export const TenantsListPage: FC = () => {
 			<UserInvitationsList />
 			<AccessibleTenantList />
 			<UserTenantsList />
+			<Messenger />
 		</>
 	);
 };
