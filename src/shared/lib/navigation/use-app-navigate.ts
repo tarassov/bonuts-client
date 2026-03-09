79d4e6faf771c73fab31071a4a36311f3ba01749
Alexander Tarasov <tarassov.al@gmail.com>
2026-03-09 15:25:47 +0300

    fixes dialog provider (#32)
    * fixes dialog provider
    
    * Fix dialog routed modal back navigation

---
+++ src/shared/lib/navigation/use-app-navigate.ts
@@ -0,0 +1,34 @@
+import { useCallback } from "react";
+import { goBack, push } from "redux-first-history";
+
+import { useAppDispatch, useAppSelector } from "services/redux/store/store";
+
+type TNavigateParams = Parameters<typeof push>;
+
+export interface ILocationProps extends Location {
+	state: {
+		from?: Location;
+		background?: Location;
+		modal?: boolean;
+		name?: string;
+		data?: any;
+	};
+}
+
+export function useAppNavigate() {
+	const dispatch = useAppDispatch();
+	const location = useAppSelector((state) => state.router.location as unknown as ILocationProps);
+
+	const navigate = useCallback(
+		(...params: TNavigateParams) => {
+			return dispatch(push(...params));
+		},
+		[dispatch]
+	);
+
+	const back = useCallback(() => {
+		return dispatch(goBack());
+	}, [dispatch]);
+
+	return { location, navigate, goBack: back };
+}
