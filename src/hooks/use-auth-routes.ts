5dbcce95c00d3c8367ab386c29df3270686d7fa3
Alexander Tarasov <tarassov.al@gmail.com>
2026-05-24 15:27:29 +0300

    hide tenants list page

--- src/hooks/use-auth-routes.ts
+++ src/hooks/use-auth-routes.ts
@@ -2,17 +2,30 @@ import { useContext, useMemo } from "react";
 
 import _ from "lodash";
 
+import { BntRoutes } from "@/shared/config/routes";
 import { RouterContext } from "@/shared/lib/router";
 import { useAuth, useCurrentProfile } from "@/shared/model/auth";
+
+import { routesPath } from "@/routes/config/routes-path";
+import { tenantsApi } from "@/services/api/extended/tenants-api";
 
 export const useAuthRoutes = () => {
 	const { menuRoutes: routes } = useContext(RouterContext);
 	const { auth } = useAuth();
 	const { currentRoles } = useCurrentProfile();
+	const { data: tenantsData } = tenantsApi.endpoints.getTenants.useQuery(undefined, {
+		skip: !auth.isAuthenticated,
+	});
+
+	const tenantCount = tenantsData?.data?.length;
+	const hasOnlyOneTenant = tenantCount === 1;
 
 	const menuRoutes = useMemo(() => {
-		return routes.filter((x) => x.tenantNotRequired || auth.tenant).filter((x) => !x.roles?.length || _.intersection(x.roles, currentRoles).length);
-	}, [routes, auth.tenant, currentRoles]);
+		return routes
+			.filter((x) => x.tenantNotRequired || auth.tenant)
+			.filter((x) => !hasOnlyOneTenant || x.path !== routesPath[BntRoutes.TenantList])
+			.filter((x) => !x.roles?.length || _.intersection(x.roles, currentRoles).length);
+	}, [routes, auth.tenant, currentRoles, hasOnlyOneTenant]);
 
 	return { menuRoutes };
 };
