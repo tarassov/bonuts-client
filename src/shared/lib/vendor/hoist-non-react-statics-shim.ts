290a68e99d960734b94418cfdaff80f2624c7141
Alexander Tarasov <tarassov.al@gmail.com>
2026-07-18 01:14:43 +0300

    revert

--- src/shared/lib/vendor/hoist-non-react-statics-shim.ts
+++
@@ -1,5 +0,0 @@
-import * as hoistNonReactStaticsModule from "../../../../node_modules/hoist-non-react-statics/dist/hoist-non-react-statics.cjs.js";
-
-const hoistNonReactStatics = "default" in hoistNonReactStaticsModule ? hoistNonReactStaticsModule.default : (hoistNonReactStaticsModule as unknown as typeof import("hoist-non-react-statics"));
-
-export default hoistNonReactStatics;
